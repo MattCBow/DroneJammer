5980448e98d4df66b3f9b59a04b04fb31dcff56c
mattcbow <mattcbow@gmail.com>
2016-03-14 04:27:36 +0000

    added helper scripts

---
+++ timetest.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import thread
+import time
+
+
+finished = 0
+# Define a function for the thread
+def print_time( threadName, delay):
+   count = 0
+   while count < 5:
+      time.sleep(delay)
+      count += 1
+      print "%s: %s" % ( threadName, time.ctime(time.time()) )
+   finished += 1
+
+# Create two threads as follows
+try:
+   thread.start_new_thread( print_time, ("Thread-1", 2, ) )
+   thread.start_new_thread( print_time, ("Thread-2", 4, ) )
+except:
+   print "Error: unable to start thread"
+
+while finish!=0:
+   pass
